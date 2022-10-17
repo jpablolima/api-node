6b502b31b2f8ed09ff89e7a950fd77d83a0e67ad
João Pablo <jp.pablolima@gmail.com>
2022-10-17 18:21:43 -0300

    obtendo dados da API de proposição de dados abertos

--- main.ts
+++ main.ts
@@ -1,10 +1,32 @@
+// Pegando dados de Proposição da API de  dados Abertos
+
+
 import axios from "axios";
 
-function GetFromCep(cep:string):Promise<any>{
-    return axios.get(`https://viacep.com.br/ws/${cep}/json/`)
-}
 
-GetFromCep('72911500').then((response) => {
-    console.log(response.data)
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req:any, res:any) => {
+    res.send('Hello, World')
 })
 
+app.get("/proposicao/:id",(req:any, res:any) => {
+    const id = req.params.id
+    axios.get(`https://dadosabertos.camara.leg.br/api/v2/proposicoes/${id}`).then((response:any) => {
+        res.json(response.data.dados)
+    })
+})
+
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`Server running in port http://localhost:${port}`)
+})
